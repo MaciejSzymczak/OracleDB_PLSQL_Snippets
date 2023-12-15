90900f888c012b76f9de60a164f388300915396d
MaciejSzymczak <soft@home.pl>
2023-12-15 07:48:49 +0100

    Update holder_waiter.sql

--- holder_waiter.sql
+++ holder_waiter.sql
@@ -300,6 +300,305 @@ and seconds_in_wait > 600 -- 10 minut
 order by seconds_in_wait desc 
 
 
+Source: https://github.com/gwenshap
+=======================================================================
+
+-- Find all blocked sessions and who is blocking them
+select sid,blocking_session,username,sql_id,event,machine,osuser,program,last_call_et from v$session where blocking_session > 0;
+
+select * from dba_blockers
+select * from dba_waiters
+
+-- Find what the blocking session is doing
+select sid,blocking_session,username,sql_id,event,state,machine,osuser,program,last_call_et from v$session where sid=746 ;
+
+-- Find the blocked objects
+select owner,object_name,object_type from dba_objects where object_id in (select object_id from v$locked_object where session_id=271 and locked_mode =3);
+
+
+-- Friendly query for who is blocking who
+-- Mostly for versions before v$session had blocking_session column
+select s1.inst_id,s2.inst_id,s1.username || '@' || s1.machine
+ || ' ( SID=' || s1.sid || ' )  is blocking '
+ || s2.username || '@' || s2.machine || ' ( SID=' || s2.sid || ' ) ' AS blocking_status
+  from gv$lock l1, gv$session s1, gv$lock l2, gv$session s2
+  where s1.sid=l1.sid and s2.sid=l2.sid and s1.inst_id=l1.inst_id and s2.inst_id=l2.inst_id
+  and l1.BLOCK=1 and l2.request > 0
+  and l1.id1 = l2.id1
+  and l2.id2 = l2.id2
+order by s1.inst_id;
+
+
+-- find blocking sessions that were blocking for more than 15 minutes + objects and sql
+select s.SID,p.SPID,s.machine,s.username,CTIME/60 as minutes_locking, do.object_name as locked_object, q.sql_text
+from v$lock l
+join v$session s on l.sid=s.sid
+join v$process p on p.addr = s.paddr
+join v$locked_object lo on l.SID = lo.SESSION_ID
+join dba_objects do on lo.OBJECT_ID = do.OBJECT_ID 
+join v$sqlarea q on  s.sql_hash_value = q.hash_value and s.sql_address = q.address
+where block=1 and ctime/60>15
+
+-- Check who is blocking who in RAC
+SELECT DECODE(request,0,'Holder: ','Waiter: ') || sid sess, id1, id2, lmode, request, type
+FROM gv$lock
+WHERE (id1, id2, type) IN (
+  SELECT id1, id2, type FROM gv$lock WHERE request>0)
+ORDER BY id1, request;
+
+-- Check who is blocking who in RAC, including objects
+SELECT DECODE(request,0,'Holder: ','Waiter: ') || gv$lock.sid sess, machine, do.object_name as locked_object,id1, id2, lmode, request, gv$lock.type
+FROM gv$lock join gv$session on gv$lock.sid=gv$session.sid and gv$lock.inst_id=gv$session.inst_id
+join gv$locked_object lo on gv$lock.SID = lo.SESSION_ID and gv$lock.inst_id=lo.inst_id
+join dba_objects do on lo.OBJECT_ID = do.OBJECT_ID 
+WHERE (id1, id2, gv$lock.type) IN (
+  SELECT id1, id2, type FROM gv$lock WHERE request>0)
+ORDER BY id1, request;
+
+
+
+
+-- Who is blocking who, with some decoding
+select	sn.USERNAME,
+	m.SID,
+	sn.SERIAL#,
+	m.TYPE,
+	decode(LMODE,
+		0, 'None',
+		1, 'Null',
+		2, 'Row-S (SS)',
+		3, 'Row-X (SX)',
+		4, 'Share',
+		5, 'S/Row-X (SSX)',
+		6, 'Exclusive') lock_type,
+	decode(REQUEST,
+		0, 'None', 
+		1, 'Null',
+		2, 'Row-S (SS)',
+		3, 'Row-X (SX)', 
+		4, 'Share', 
+		5, 'S/Row-X (SSX)',
+		6, 'Exclusive') lock_requested,
+	m.ID1,
+	m.ID2,
+	t.SQL_TEXT
+from 	v$session sn, 
+	v$lock m , 
+	v$sqltext t
+where 	t.ADDRESS = sn.SQL_ADDRESS 
+and 	t.HASH_VALUE = sn.SQL_HASH_VALUE 
+and 	((sn.SID = m.SID and m.REQUEST != 0) 
+or 	(sn.SID = m.SID and m.REQUEST = 0 and LMODE != 4 and (ID1, ID2) in
+        (select s.ID1, s.ID2 
+         from 	v$lock S 
+         where 	REQUEST != 0 
+         and 	s.ID1 = m.ID1 
+         and 	s.ID2 = m.ID2)))
+order by sn.USERNAME, sn.SID, t.PIECE
+
+-- Who is blocking who, with some decoding
+select	OS_USER_NAME os_user,
+	PROCESS os_pid,
+	ORACLE_USERNAME oracle_user,
+	l.SID oracle_id,
+	decode(TYPE,
+		'MR', 'Media Recovery',
+		'RT', 'Redo Thread',
+		'UN', 'User Name',
+		'TX', 'Transaction',
+		'TM', 'DML',
+		'UL', 'PL/SQL User Lock',
+		'DX', 'Distributed Xaction',
+		'CF', 'Control File',
+		'IS', 'Instance State',
+		'FS', 'File Set',
+		'IR', 'Instance Recovery',
+		'ST', 'Disk Space Transaction',
+		'TS', 'Temp Segment',
+		'IV', 'Library Cache Invalidation',
+		'LS', 'Log Start or Switch',
+		'RW', 'Row Wait',
+		'SQ', 'Sequence Number',
+		'TE', 'Extend Table',
+		'TT', 'Temp Table', type) lock_type,
+	decode(LMODE,
+		0, 'None',
+		1, 'Null',
+		2, 'Row-S (SS)',
+		3, 'Row-X (SX)',
+		4, 'Share',
+		5, 'S/Row-X (SSX)',
+		6, 'Exclusive', lmode) lock_held,
+	decode(REQUEST,
+		0, 'None',
+		1, 'Null',
+		2, 'Row-S (SS)',
+		3, 'Row-X (SX)',
+		4, 'Share',
+		5, 'S/Row-X (SSX)',
+		6, 'Exclusive', request) lock_requested,
+	decode(BLOCK,
+		0, 'Not Blocking',
+		1, 'Blocking',
+		2, 'Global', block) status,
+	OWNER,
+	OBJECT_NAME
+from	v$locked_object lo,
+	dba_objects do,
+	v$lock l
+where 	lo.OBJECT_ID = do.OBJECT_ID
+AND     l.SID = lo.SESSION_ID
+and block=1
+
+
+
+
+Source: https://github.com/gwenshap
+=======================================================================
+
+-- Check what the sessions in our instance are waiting for
+select event,count(*) from v$session group by event order by count(*);
+
+-- Flexible query to check what's currently running in the system
+-- Where statement and column lists can be modified by the case
+-- Written for RAC DBs
+select 
+s.inst_id,
+--      'alter system kill session '''|| s.SID||',' || s.serial# ||'''' ,
+--'!kill -9 ' || p.spid, 
+      p.SPID UnixProcess ,s.SID,s.serial#,s.USERNAME,s.COMMAND,s.MACHINE,s.blocking_session
+      ,s.program, status,state,event,s.sql_id,sql_text,COMMAND_TYPE
+--    ,sbc.name,to_char(sbc.last_captured,'yyyy-mm-dd hh24:mi:ss'),sbc.value_string
+    from gv$session s
+left outer join gv$process p on p.ADDR = s.PADDR and s.inst_id=p.inst_id 
+left outer join gv$sqlarea sa on sa.ADDRESS = s.SQL_ADDRESS and s.inst_id=sa.inst_id
+--left outer join gV$SQL_BIND_CAPTURE sbc on sbc.ADDRESS = s.SQL_ADDRESS and s.inst_id=p.inst_id
+where 1=1 and sql_text like '...'
+
+-- Check what a specific session is doing:
+select 
+      p.SPID UnixProcess ,s.SID,s.serial#,s.USERNAME,s.COMMAND,s.MACHINE,s.SQL_ADDRESS,s.SQL_HASH_VALUE
+      ,s.program, status,sql_text,COMMAND_TYPE
+    from gv$session s,gv$process p, gv$sqlarea sa
+	where p.ADDR = s.PADDR and s.inst_id=p.inst_id 
+	and sa.ADDRESS = s.SQL_ADDRESS and s.inst_id=sa.inst_id
+	and s.sid=1722;
+
+-- Find all sessions that are blocked and which session is blocking them
+-- Then find what the blocking session is doing
+select sid,blocking_session,username,sql_id,event,machine,osuser,program from v$session where blocking_session > 0;
+select sid,blocking_session,username,sql_id,event,machine,osuser,program from v$session where sid=491;
+
+
+-- generate commands to kill all sessions from a specific user on specific instance
+select 'alter system kill session '''|| SID||',' || serial# ||''' immediate;' from gv$session where username='BAD_USER' and inst_id=1;
+
+
+-- Kill all sessions waiting for specific events by a specific user
+select       'alter system kill session '''|| s.SID||',' || s.serial# ||''';' 
+from gv$session s
+where 1=1 
+and (event='latch: shared pool' or event='library cache lock') and s.USERNAME='DBSNMP';
+
+-- kill all sessions executing a bad SQL
+select 
+     'alter system kill session '''|| s.SID||',' || s.serial# ||''';' 
+    from v$session s
+where s.sql_id='0vj44a7drw1rj';
+
+
+-- Sessions taking most PGA memory
+-- Can be used to find leaks
+select addr,SPID,username,program,pga_alloc_mem/1024 mem_alloc_Kb from v$process order by pga_alloc_mem;
+
+-- Check what is the top SQL executed by parallel slaves
+select sql_id,count(*) from v$session where program like '%P0%' group by sql_id;
+
+
+
+-- Find inactive sessions
+-- This can be used to decide which sessions to kill if the DB is running out of processes
+select sid, blocking_session,username,program,machine,osuser,  sql_id, prev_sql_id, event,LAST_CALL_ET from v$session where status != 'ACTIVE' and last_call_et>3600;
+
+
+-- How many sessions openned by each app server
+select machine,count(*) from gv$session s group by machine;
+
+-- Find sql_id for a specific sql snippet
+select sql_id,sql_text from v$sql where dbms_lob.instr(sql_text, 'create INDEX',1,1) > 0
+
+-- Find SQL with too many child cursors:
+select version_count,sql_text from v$sqlarea order by version_count desc
+
+
+
+-- Get the longops status for a specific session
+	select sid
+	,      message, start_time,time_remaining 
+	from   v$session_longops
+	where  sid = 28
+	order by start_time;
+
+-- Check status for long ops executing right now
+  select s.sid,s.serial#,opname, target, program,sofar, totalwork,units, elapsed_seconds, message,start_time,time_remaining   
+from v$session_longops l
+join v$session s on l.sid=s.sid and s.serial#=l.serial#
+where time_remaining>0
+order by start_time desc
+
+
+
+
+-- Find out how much memory each session is using
+COLUMN sid                     FORMAT 999            HEADING 'SID'
+COLUMN oracle_username         FORMAT a12            HEADING 'Oracle User'     JUSTIFY right
+COLUMN os_username             FORMAT a9             HEADING 'O/S User'        JUSTIFY right
+COLUMN session_program         FORMAT a18            HEADING 'Session Program' TRUNC
+COLUMN session_module         FORMAT a18            HEADING 'Session module' TRUNC
+COLUMN session_action         FORMAT a18            HEADING 'Session action' TRUNC
+COLUMN session_machine         FORMAT a8             HEADING 'Machine'   JUSTIFY right TRUNC
+COLUMN session_pga_memory      FORMAT 9,999,999,999  HEADING 'PGA Memory'
+COLUMN session_pga_memory_max  FORMAT 9,999,999,999  HEADING 'PGA Memory Max'
+COLUMN session_uga_memory      FORMAT 9,999,999,999  HEADING 'UGA Memory'
+COLUMN session_uga_memory_max  FORMAT 9,999,999,999  HEADING 'UGA Memory MAX'
+COLUMN session_total_memory  FORMAT 9,999,999,999  HEADING 'Total Memory'
+
+select sid,oracle_username,os_username,session_program,session_module,session_action, 
+session_pga_memory,session_pga_memory_max,session_uga_memory,session_uga_memory_max,session_pga_memory+session_uga_memory session_total_memory from (
+SELECT
+    s.sid                sid
+  , lpad(s.username,12)  oracle_username
+  , lpad(s.osuser,9)     os_username
+  , s.program            session_program
+  , s.module            session_module
+  , s.action            session_action
+  , lpad(s.machine,8)    session_machine
+  , (select ss.value from v$sesstat ss, v$statname sn
+     where ss.sid = s.sid and 
+           sn.statistic# = ss.statistic# and
+           sn.name = 'session pga memory')        session_pga_memory
+  , (select ss.value from v$sesstat ss, v$statname sn
+     where ss.sid = s.sid and 
+           sn.statistic# = ss.statistic# and
+           sn.name = 'session pga memory max')    session_pga_memory_max
+  , (select ss.value  from v$sesstat ss, v$statname sn
+     where ss.sid = s.sid and 
+           sn.statistic# = ss.statistic# and
+           sn.name = 'session uga memory')        session_uga_memory
+  , (select ss.value from v$sesstat ss, v$statname sn
+     where ss.sid = s.sid and 
+           sn.statistic# = ss.statistic# and
+           sn.name = 'session uga memory max')   as session_uga_memory_max
+FROM 
+    v$session  s )
+ORDER BY session_total_memory DESC;
+
+
+
+
+
+
+
 
 
 
